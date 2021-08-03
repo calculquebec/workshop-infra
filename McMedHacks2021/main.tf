bbaff0d25f75fc4b0e7a9892d179e4e316ac96e7
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-08-03 16:03:29 -0400

    Update main.tf

--- McMedHacks2021/main.tf
+++ McMedHacks2021/main.tf
@@ -8,7 +8,7 @@ variable "email" {}
 module "openstack" {
   source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref=11.2"
   config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
-  config_version = "11.2"
+  config_version = "11.3"
 
   cluster_name = "mcmedhacks2021"
   domain       = "calculquebec.cloud"
